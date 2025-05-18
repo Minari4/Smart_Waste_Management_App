613d7cc893e8d68e6fa92538ae528c27f9041679
Minari4 <alminaadlawon283@gmail.com>
2025-05-18 17:38:42 +0800

    Update build script to create superuser

--- build.sh
+++ build.sh
@@ -11,4 +11,12 @@ cd waste_management
 python manage.py collectstatic --no-input
 
 # Run migrations
-python manage.py migrate 
+python manage.py migrate
+
+# Create superuser
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+EOF 
